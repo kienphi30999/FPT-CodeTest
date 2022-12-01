8a3f87fe0b7afb854815f3e128916bdecd2a87cb
Lý Kiến Phi <53138234+kienphi30999@users.noreply.github.com>
2022-12-01 15:46:27 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
